733e59918820e142b4dd6264734be6aafc3d3c11
IIIIIIIIIIIIIIIIIIII <vchug322@gmail.com>
2023-02-06 02:12:09 +0300

    1

---
+++ 123.py
@@ -0,0 +1,2 @@
+def ass():
+    print('hello')
---
+++ main.py
@@ -0,0 +1,17 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return 'index'
+
+
+@app.route('/about')
+def about():
+    return 'about'
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
